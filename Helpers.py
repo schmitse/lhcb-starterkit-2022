efa37a743bc7680fb3686f0fe594e48952b635d6
Sebastian Schmitt <sebastian.schmitt1@rwth-aachen.de>
2022-11-21 11:51:29 +0100

    add submodel plots and update cost function description

--- Helpers.py
+++ Helpers.py
@@ -6,7 +6,7 @@ import zfit
 def plot_fit(dat: np.ndarray, basis: np.ndarray, model: np.ndarray, 
              obs: zfit.Space, nbins : int=50, smodel: np.ndarray=None,
              drawstyle: str='default', weight: np.ndarray=None,
-             xlabel: str='Observable'):
+             xlabel: str='Observable', zmodel: zfit.pdf.BasePDF=None):
     """
     quick plotting function to visualise data and model. 
     Takes:
@@ -19,6 +19,7 @@ def plot_fit(dat: np.ndarray, basis: np.ndarray, model: np.ndarray,
      - drawstyle: (str) the drawstyle of plt.plot
      - weight: (array) optional weight array for the data
      - xlabel: (str) label for the x axis
+     - zmodel: (zfit PDF) model for drawing submodels
     Returns:
      - None
     """
@@ -47,9 +48,27 @@ def plot_fit(dat: np.ndarray, basis: np.ndarray, model: np.ndarray,
                                (model-smodel)*area/nbins, color='darkturquoise', 
                                alpha=0.5, zorder=-2)
         art_model = (art_model, _art)
+
+    # define artists and labels for the legend
+    artists = [art_data, art_model]
+    labels = ['Data', 'Model']
+    # if we want to plot the submodels of our model, we can iterate through
+    # all of them and evaluate them at our basis. We will not bootstrap
+    # all of their shape uncertainties though, this is just an illustration
+    if zmodel is not None:
+        nmodels = len(zmodel.get_models())
+        cmap = plt.get_cmap('autumn')
+        norm = mpl.colors.Normalize(0, nmodels)
+        pdfs = [(m.pdf(basis)*m.get_yield()).numpy()*area/nbins
+                for m in zmodel.get_models()]
+        names = [m.name.replace('_extended','') for m in zmodel.get_models()]
+        labels.extend(names)
+        for mdex, pdf in enumerate(pdfs):
+            artists.append(ax.plot(basis, pdf, color=cmap(norm(mdex)), 
+                                   linestyle='--')[0])
         
     # legend and axis labels
-    ax.legend((art_data, art_model), ('Data', 'Model'), loc='best', 
+    ax.legend(artists, labels, loc='best', 
               title='LHCb Starterkit', title_fontsize=12)
     ax.set_xlabel(xlabel)
     ax.set_ylabel('Counts [a.u.]');
